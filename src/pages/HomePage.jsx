9a224388355462548982ee0418ceb2c1eef01dd2
ANMD3173 <anmd3173@gmail.com>
2024-12-20 17:43:42 +0500

    work on gradient

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,47 +7,68 @@ import TeamHome from "../components/HomeComponent/TeamHome/TeamHome";
 import ClientHome from "../components/HomeComponent/ClientHome/ClientHome";
 const Home = () => {
   return (
-    <>
-      <div className="row Container" style={{position:'relative'}}>
+    <div style={{ overflow: "hidden" }}>
+      <div className="row Container" style={{ position: "relative" }}>
         <div className="col-12">
           <div className="row">
             <div className="col-12" style={{}}>
               <HeroSection />
             </div>
           </div>
-          <div className="row " style={{marginTop:"100px"}}>
+          <div className="row " style={{ marginTop: "100px" }}>
             <div className="col-12 ">
-               <AboutHome/>
+              <AboutHome />
             </div>
           </div>
-          <div className="row " style={{marginTop:"100px"}}>
+          <div className="row " style={{ marginTop: "100px" }}>
             <div className="col-12 ">
-               <ServicesHome/>
+              <ServicesHome />
             </div>
           </div>
-          <div className="row " style={{marginTop:"100px"}}>
+          <div className="row " style={{ marginTop: "100px" }}>
             <div className="col-12 ">
-               <TeamHome/>
+              <TeamHome />
             </div>
           </div>
-          <div className="row " style={{marginTop:"100px"}}>
+          <div className="row " style={{ marginTop: "100px" }}>
             <div className="col-12 ">
-               <ClientHome/>
+              <ClientHome />
             </div>
           </div>
         </div>
 
-        <div className="gradient_radial" style={{height:'950px',width:'950px', position:'absolute' , top:'-450px',left:'-20px' }}>
-                
-        </div>
-        <div className="gradient_radial" style={{height:'950px',width:'950px', position:'absolute' , top:'200px',left:'-400px' }}>
-                
-        </div>
-        <div className="gradient_radial" style={{height:'950px',width:'950px', position:'absolute', top:'500px' ,right:'-400px' }}>
-                
-                </div>
+        <div
+          className="gradient_radial"
+          style={{
+            height: "950px",
+            width: "950px",
+            position: "absolute",
+            top: "-100px",
+            left: "-20px",
+          }}
+        ></div>
+        <div
+          className="gradient_radial"
+          style={{
+            height: "950px",
+            width: "950px",
+            position: "absolute",
+            top: "200px",
+            left: "-400px",
+          }}
+        ></div>
+        <div
+          className="gradient_radial"
+          style={{
+            height: "950px",
+            width: "950px",
+            position: "absolute",
+            top: "500px",
+            right: "-400px",
+          }}
+        ></div>
       </div>
-    </>
+    </div>
   );
 };
 export default Home;
